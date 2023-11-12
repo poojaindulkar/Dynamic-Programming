d780e0c607b969fbdb63bab8bcb529c644de2199
Pooja Indulkar <poojaindulkar200@gmail.com>
2023-11-12 21:13:56 +0530

    LSC, Coin Combination

---
+++ LCS/lcs.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Recursion
+/*
+int LCS(string x, string y, int n, int m){
+    if(n==0 || m==0) return 0;
+    if(x[n-1]==y[m-1]) return 1+LCS(x,y,n-1,m-1);
+    return max(LCS(x,y,n-1,m),LCS(x,y,n,m-1));
+}
+int main(){
+    
+        string x,y;
+        cin>>x>>y;
+        cout<<LCS(x,y,x.length(),y.length());
+    
+    return 0;
+}
+*/
+
+//Memorization
+/*
+int dp[100][100];
+int LCS(string x, string y, int n, int m){
+    if(n==0 || m==0) return 0;
+    if(dp[n][m]!=-1) return dp[n][m];
+    if(x[n-1]==y[m-1]) 
+        return dp[n][m]=1+LCS(x,y,n-1,m-1);
+    return dp[n][m]=max(LCS(x,y,n-1,m),LCS(x,y,n,m-1));
+}
+int main(){
+   
+        string x,y;
+        cin>>x>>y;
+        for(int i=0;i<=x.length();i++){
+            for(int j=0;j<=y.length();j++){
+                dp[i][j]=-1;
+            }
+        }
+        cout<<LCS(x,y,x.length(),y.length());
+ 
+    return 0;
+}
+*/
+
+//Bottom-up
+int LCS(string x, string y){
+    int n=x.length(),m=y.length();
+    int dp[n+1][m+1];
+    for(int i=0;i<=n;i++){
+        dp[i][0]=0;
+    }
+    for(int i=0;i<=m;i++){
+        dp[0][i]=0;
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(x[i-1]==y[j-1]){
+                dp[i][j]=1 + dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return dp[n][m];
+}
+int main(){
+   
+        string x,y;
+        cin>>x>>y;
+        cout<<LCS(x,y);
+ 
+    return 0;
+}
---
+++ unbounded_knapsack/coin_combination_I.cpp
@@ -0,0 +1,59 @@
+/*
+Consider a money system consisting of n coins. Each coin has a positive integer value. Your task is to calculate the number of distinct ways you can produce a money sum x using the available coins.
+For example, if the coins are \{2,3,5\} and the desired sum is 9, there are 8 ways:
+
+2+2+5
+2+5+2
+5+2+2
+3+3+3
+2+2+2+3
+2+2+3+2
+2+3+2+2
+3+2+2+2
+
+Input
+The first input line has two integers n and x: the number of coins and the desired sum of money.
+The second line has n distinct integers c_1,c_2,\dots,c_n: the value of each coin.
+Output
+Print one integer: the number of ways modulo 10^9+7.
+
+*/
+
+#include <bits/stdc++.h> 
+using namespace std;
+int mod=1000000007;
+int coinCombinationI(vector<int> &num, int x)
+{
+    int n=num.size();
+    int dp[n+1][x+1];
+    //Initilizing 
+    for(int i=0;i<=x;i++){
+        dp[0][i]=0;
+    }
+    for(int i=0;i<=n;i++){
+        dp[i][0]=1;
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=x;j++){
+            if(num[i-1]<=j){
+                dp[i][j]=dp[i-1][j] + dp[i][j-num[i-1]];
+            }
+         
+        }
+        
+    }
+    return dp[n][x];
+
+}
+
+int main(){
+    vector<int>v;
+    int n,x;
+    cin>>n>>x;
+    for(int i=0;i<n;i++){
+        int tmp;
+        cin>>tmp;
+        v.push_back(tmp);
+    }
+    cout<<coinCombinationI(v,x);
+}
